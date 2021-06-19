d4dec2db16ad6eea31e347093948a835fe13d7b7
lucasfeitozas <lucasfeitozas@gmail.com>
2021-06-18 22:32:26 -0300

    add test cases to createCarSpecificationsUseCase

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,5 +1,25 @@
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+  car_id: string;
+  specifications_id: string[];
+}
+
 class CreateCarSpecificationUseCase {
-  async execute(): Promise<void> {}
+  constructor(
+    private carsRepository: ICarsRepository,
+    private specificationsRepository: ISpecificationsRepository
+  ) {}
+
+  async execute({ car_id, specifications_id }: IRequest): Promise<void> {
+    const carExists = await this.carsRepository.findById(car_id);
+
+    if (!carExists) {
+      throw new AppError("Carro nao existe");
+    }
+  }
 }
 
 export { CreateCarSpecificationUseCase };
